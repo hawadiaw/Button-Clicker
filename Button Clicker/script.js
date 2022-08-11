a3e029213420c7cea0073c8c52f15667305078a6
hawadiaw <75343133+hawadiaw@users.noreply.github.com>
2022-08-11 14:02:24 -0400

    Add files via upload

---
+++ Button Clicker/script.js
@@ -0,0 +1,19 @@
+function login(element) {
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function remove (element){
+    element.remove();
+}
+
+function message(){
+ alert('Ninja was liked');
+}
+
+function name(){
+    alert("Ninja was liked!");
+}
